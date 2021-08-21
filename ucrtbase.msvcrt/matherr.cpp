c1a36bd152b34523097a59bf5dfb538c4cbd6c1f
mingkuang <mingkuang@live.com>
2021-08-21 17:48:45 +0800

    解决__acrt_has_user_matherr符号找不到问题（感谢 毛利）（https://github.com/mingkuang-Chuyu/VC-LTL5/issues/2）

---
+++ ucrtbase.msvcrt/matherr.cpp
@@ -0,0 +1,48 @@
+﻿
+
+#include <internal_shared.h>
+#include "msvcrt_loadhelper.h"
+
+typedef int(__cdecl* _HANDLE_MATH_ERROR)(struct _exception*);
+
+static _HANDLE_MATH_ERROR user_matherr;
+
+extern "C" void __cdecl __setusermatherr(
+	_HANDLE_MATH_ERROR pf
+	)
+{
+	//继续调用msvcrt版本是为了能接管 msvcrt的user_matherr，但是有一个问题，如果其他人调用了 __setusermatherr，这会覆盖msvcrt的user_matherr
+	//二者会发生不一致……这类问题无法一种健全的解决方案，但是可以暂不处理
+	__EXPAND_MSVCRT_FUN(__setusermatherr);
+
+	if (auto p_setusermatherr_msvcrt = __Get_MSVCRT_FUN(__setusermatherr))
+	{
+		p_setusermatherr_msvcrt(pf);
+	}
+
+	user_matherr = __crt_fast_encode_pointer(pf);
+}
+
+_LCRT_DEFINE_IAT_SYMBOL(__setusermatherr);
+
+
+extern "C" bool __cdecl __acrt_has_user_matherr()
+{
+	return user_matherr != nullptr && __crt_fast_decode_pointer(user_matherr);
+}
+
+extern "C" int __cdecl __acrt_invoke_user_matherr(struct _exception* ex)
+{
+	auto tmp = user_matherr;
+
+	if (tmp)
+	{
+		tmp = __crt_fast_decode_pointer(tmp);
+
+		if (tmp)
+			return tmp(ex);
+	}
+	
+
+	return 0;
+}
