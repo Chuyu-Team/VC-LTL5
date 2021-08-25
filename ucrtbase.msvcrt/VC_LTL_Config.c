557acd8ecd8f975e82ca4f7cb0ffc2b80a1dad3b
mingkuang <mingkuang@live.com>
2021-08-25 16:55:19 +0800

    __LTL_****_module_type添加const属性，防止修改

--- ucrtbase.msvcrt/VC_LTL_Config.c
+++ ucrtbase.msvcrt/VC_LTL_Config.c
@@ -1,3 +1,3 @@
 ﻿#include <VC_LTL_Config.h>
 
-int __LTL_ucrt_module_type = _LTL_ucrt_module_type_from_msvcrt;
+const int __LTL_ucrt_module_type = _LTL_ucrt_module_type_from_msvcrt;
--- vcruntime.msvcrt/VC_LTL_Config.c
+++ vcruntime.msvcrt/VC_LTL_Config.c
@@ -1,3 +1,3 @@
 ﻿#include <VC_LTL_Config.h>
 
-int __LTL_vcruntime_module_type = _LTL_vcruntime_module_type_from_msvcrt;
+const int __LTL_vcruntime_module_type = _LTL_vcruntime_module_type_from_msvcrt;
