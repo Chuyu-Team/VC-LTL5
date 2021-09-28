6d42dea3f2c4709e46149a24515691162e67c4bf
MouriNaruto <Mouri_Naruto@Outlook.com>
2021-09-28 16:27:41 +0800

    Add version information support for Rust language support.

--- Rust/src/lib.rs
+++ Rust/src/lib.rs
@@ -1,7 +1,14 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+/*
+ * PROJECT:   Rust language support for VC-LTL
+ * FILE:      lib.rs
+ * PURPOSE:   Implementation for VC-LTL information
+ *
+ * LICENSE:   The MIT License
+ *
+ * DEVELOPER: Mouri_Naruto (Mouri_Naruto AT Outlook.com)
+ */
+
+pub const VERSION_MAJOR: i32 = 5;
+pub const VERSION_MINOR: i32 = 0;
+pub const VERSION_BUILD: i32 = 1;
+pub const VERSION_REVISION: i32 = 4;
