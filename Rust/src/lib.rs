c42e6a041061be5ce637cd94021b541a451bbcb3
MouriNaruto <Mouri_Naruto@Outlook.com>
2021-10-11 16:52:09 +0800

    添加 VC-LTL 的 Rust 语言支持

---
+++ Rust/src/lib.rs
@@ -0,0 +1,9 @@
+/*
+ * PROJECT:   Rust language support for VC-LTL
+ * FILE:      lib.rs
+ * PURPOSE:   Implementation for VC-LTL information
+ *
+ * LICENSE:   The MIT License
+ *
+ * DEVELOPER: Mouri_Naruto (Mouri_Naruto AT Outlook.com)
+ */
