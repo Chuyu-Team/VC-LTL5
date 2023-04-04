1ad6ed0394a0508fe13283bba17d4a98b601954a
mingkuang <mingkuang@live.com>
2023-04-05 00:52:24 +0800

    Bug #45, 解决5.2.3790.0编译目标在Windows Server 2003 x64上无法运行

---
+++ ucrtbase.msvcrt/tanhf.cpp
@@ -0,0 +1,18 @@
+﻿#include <math.h>
+
+#pragma warning(disable : 28251)
+
+#if defined _M_X64 || defined _M_ARM || defined _M_ARM64 || defined _M_HYBRID_X86_ARM64 || defined _M_ARM64EC
+#if WindowsTargetPlatformMinVersion < WindowsTargetPlatformWindows6
+#pragma function(tanhf)
+
+extern "C" float __CRTDECL tanhf(float _X)
+{
+    // 2003的DLL有 double版，凑合一下吧。
+    return (float)tanh(_X);
+}
+
+_LCRT_DEFINE_IAT_SYMBOL(tanhf);
+
+#endif // WindowsTargetPlatformMinVersion < WindowsTargetPlatformWindows6
+#endif // defined _M_X64 || defined _M_ARM || defined _M_ARM64 || defined _M_HYBRID_X86_ARM64 || defined _M_ARM64EC
